9e5bf90dcd5c7fd4b0d4d758dd451aad9b265403
cjhill <cjhill0216@gmail.com>
2015-03-02 21:22:02 -0500

    Index File

---
+++ index.php
@@ -0,0 +1,23 @@
+<!-- Include Header -->
+<?php include 'header.php'; ?>
+
+<!-- Navigation -->
+<?php //include 'nav.php'; ?>
+
+<!-- Jumbotron -->
+<?php include 'jumbotron.php'; ?>
+
+<!-- About -->
+<?php include 'about.php'; ?>
+
+<!-- Email -->
+<?php //include 'email.php'; ?>
+
+<!-- Blog -->
+<?php include 'blog.php'; ?>
+
+<!-- Videos -->
+<?php include 'videos.php'; ?>
+
+<!-- Include Footer -->
+<?php include 'footer.php'; ?>
